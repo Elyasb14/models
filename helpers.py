a5494b344b2e7cc0d805cd581e320684bd947658
Elyas Bianchi <elyasb99@gmail.com>
2023-12-07 05:33:18 +0000

    heading to bed

--- helpers.py
+++ helpers.py
@@ -14,10 +14,10 @@ def plot_mnist(idx):
 
 def load_mnist(tensors=False) -> Tuple[np.ndarray]:
   def parse(file): return np.frombuffer(file, dtype=np.uint8).copy() # gives 1d array of 64 random integers
-  data_dir = sorted(os.listdir("data"))
-  with open(f"data/{data_dir[0]}", "rb") as f: TEST_IM = parse(f.read())[0x10:].reshape((-1, 28*28)).astype(np.float32)
-  with open(f"data/{data_dir[1]}", "rb") as f: TEST_LAB = parse(f.read())[8:]
-  with open(f"data/{data_dir[2]}", "rb") as f: TRAIN_IM = parse(f.read())[0x10:].reshape((-1, 28*28)).astype(np.float32)
-  with open(f"data/{data_dir[3]}", "rb") as f: TRAIN_LAB = parse(f.read())[8:]
+  data_dir = sorted(os.listdir("data/fashionmnist"))
+  with open(f"data/fashionmnist/{data_dir[0]}", "rb") as f: TEST_IM = parse(f.read())[0x10:].reshape((-1, 28*28)).astype(np.float32)
+  with open(f"data/fashionmnist/{data_dir[1]}", "rb") as f: TEST_LAB = parse(f.read())[8:]
+  with open(f"data/fashionmnist/{data_dir[2]}", "rb") as f: TRAIN_IM = parse(f.read())[0x10:].reshape((-1, 28*28)).astype(np.float32)
+  with open(f"data/fashionmnist/{data_dir[3]}", "rb") as f: TRAIN_LAB = parse(f.read())[8:]
   if tensors: return Tensor(TRAIN_IM).reshape(-1, 1, 28, 28), Tensor(TRAIN_LAB), Tensor(TEST_IM).reshape(-1, 1, 28, 28), Tensor(TEST_LAB)   # noqa: E701
   return TEST_IM, TEST_LAB, TRAIN_IM, TRAIN_LAB
