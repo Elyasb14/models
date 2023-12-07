6f3a732c22ea8ce97fbd6c66c3559608a65ec0e5
Elyas Bianchi <elyasb99@gmail.com>
2023-12-07 00:10:05 +0000

    my AI is so advanced

--- mnist.py
+++ mnist.py
@@ -3,15 +3,15 @@ from tinygrad.helpers import dtypes, Timing
 from tinygrad.nn import Linear
 import numpy as np
 from tinygrad.nn.optim import SGD
-from tinygrad.nn.state import get_parameters
-from helpers import load_mnist
+from tinygrad.nn.state import get_parameters, safe_save, safe_load, get_state_dict, load_state_dict
+from helpers import load_mnist, plot_mnist
+import matplotlib.pyplot as plt
 
 def sparse_categorical_crossentropy(self, Y, ignore_index=-1) -> Tensor:
     loss_mask = Y != ignore_index
     y_counter = Tensor.arange(self.shape[-1], dtype=dtypes.int32, requires_grad=False, device=self.device).unsqueeze(0).expand(Y.numel(), self.shape[-1])
     y = ((y_counter == Y.flatten().reshape(-1, 1)).where(-1.0, 0) * loss_mask.reshape(-1, 1)).reshape(*Y.shape, self.shape[-1])
     return self.log_softmax().mul(y).sum() / loss_mask.sum()
-
 
 TEST_IM, TEST_LAB, TRAIN_IM, TRAIN_LAB = load_mnist()
 
@@ -27,14 +27,14 @@ class TinyNet:
     x = self.l2(x)
     return x
   
-def train():
+def train(steps: int):
   net = TinyNet() 
   opt = SGD(get_parameters(net), lr=3e-4) # can also do SGD([net.l1.weight, net.l2.weight], lr=3e-4)
   with Tensor.train():
-    for step in range(1000):
+    for step in range(steps):
       samp = np.random.randint(0, TRAIN_IM.shape[0], size=(64))
       batch = Tensor(TRAIN_IM[samp], requires_grad=False) # grad is false because we don't need to compute gradients on these tensors
-      labels = Tensor(TRAIN_LAB[samp])
+      labels = Tensor(TRAIN_LAB[samp], requires_grad=False)
       out = net(batch) # forward pass
       loss = sparse_categorical_crossentropy(out, labels) # TODO: it would be nice to implement this myself, i don't really know what is going on here tbh 
       opt.zero_grad()
@@ -46,14 +46,23 @@ def train():
         print(f"Step {step+1} | Loss: {loss.numpy()} | Accuracy: {acc.numpy() * 100}") # prints step, loss, and accuracy
   with Timing("Time: "):
     avg_acc = 0
-    for step in range(1000):
+    for step in range(steps):
       samp = np.random.randint(0, TEST_IM.shape[0], size=(64))
       batch = Tensor(TEST_IM[samp], requires_grad=False)
       labels = TEST_LAB[samp]
-      out =net(batch)
+      out = net(batch)
       pred = out.argmax(axis=-1).numpy()
       avg_acc += (pred == labels).mean()
     print(f"Test Accuracy: {avg_acc/1000}")
+  # state_dict = get_state_dict(net)
+  # safe_save(state_dict, "model.safetensors")
+  # load_state_dict(net, state_dict)
+  example = batch[1]
+  example_label = labels[1]
+  prediction = net(example).argmax(axis=-1)
+  print(example_label, prediction.numpy())
+  plt.imshow(example.numpy().reshape((28,28)))
+  plt.savefig("plot")
 
 if __name__ == "__main__":
-  train()
+  train(1000)
