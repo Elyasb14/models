b091c2a936d74d273373fab353149f00eadc51bc
Elyas Bianchi <elyasb99@gmail.com>
2023-12-08 18:04:04 +0000

    loss plot

--- helpers.py
+++ helpers.py
@@ -19,7 +19,7 @@ def load_fashion(tensors=False) -> Tuple[np.ndarray]:
   with open(f"data/fashionmnist/{data_dir[2]}", "rb") as f: TRAIN_IM = parse(f.read())[0x10:].reshape((-1, 28*28)).astype(np.float32)
   with open(f"data/fashionmnist/{data_dir[3]}", "rb") as f: TRAIN_LAB = parse(f.read())[8:]
   if tensors: return Tensor(TRAIN_IM).reshape(-1, 1, 28, 28), Tensor(TRAIN_LAB), Tensor(TEST_IM).reshape(-1, 1, 28, 28), Tensor(TEST_LAB)   # noqa: E701
-  return TEST_IM, TEST_LAB, TRAIN_IM, TRAIN_LAB 
+  return TEST_IM, TEST_LAB, TRAIN_IM, TRAIN_LAB
 
 def load_mnist(tensors=False) -> Tuple[np.ndarray]:
   def parse(file): return np.frombuffer(file, dtype=np.uint8).copy() # gives 1d array of 64 random integers
--- mnist.py
+++ mnist.py
@@ -4,7 +4,7 @@ from tinygrad.nn import Linear
 import numpy as np
 from tinygrad.nn.optim import SGD
 from tinygrad.nn.state import get_parameters, safe_save, safe_load, get_state_dict, load_state_dict
-from helpers import load_fashion, plot_mnist
+from helpers import load_fashion, load_mnist
 import matplotlib.pyplot as plt
 from random import randint
 
@@ -14,22 +14,23 @@ def sparse_categorical_crossentropy(self, Y, ignore_index=-1) -> Tensor:
     y = ((y_counter == Y.flatten().reshape(-1, 1)).where(-1.0, 0) * loss_mask.reshape(-1, 1)).reshape(*Y.shape, self.shape[-1])
     return self.log_softmax().mul(y).sum() / loss_mask.sum()
 
-TEST_IM, TEST_LAB, TRAIN_IM, TRAIN_LAB = load_fashion()
+TEST_IM, TEST_LAB, TRAIN_IM, TRAIN_LAB = load_mnist()
 
 class TinyNet:
   def __init__(self):
     self.l1 = Linear(784, 128, bias=False)
     self.l2 = Linear(128, 10, bias=False)
-  
+
   def __call__(self, x: Tensor) -> Tensor:
     x = self.l1(x)
     x = x.leakyrelu()
     x = self.l2(x)
     return x
-  
+
 def train(steps=10):
   net = TinyNet() 
   opt = SGD(get_parameters(net), lr=3e-4) # can also do SGD([net.l1.weight, net.l2.weight], lr=3e-4)
+  loss_list =[]
   with Tensor.train():
     for step in range(steps):
       samp = np.random.randint(0, TRAIN_IM.shape[0], size=(64))
@@ -42,6 +43,7 @@ def train(steps=10):
       opt.step() # this updates the parameters
       pred = out.argmax(axis=-1) # this basically return the model's prediction 
       acc = (pred == labels).mean() # i'm having a really hard time understanding what's going on here
+      loss_list.append(loss)
       if step % 100 == 0:
         print(f"Step {step+1} | Loss: {loss.numpy()} | Accuracy: {acc.numpy() * 100}") # prints step, loss, and accuracy
   with Timing("Time: "):
@@ -53,9 +55,15 @@ def train(steps=10):
       out = net(batch)
       pred = out.argmax(axis=-1).numpy()
       avg_acc += (pred == labels).mean()
-    print(f"Test Accuracy: {avg_acc/1000}")
+    print(f"Test Accuracy: {avg_acc/steps}")
   state_dict = get_state_dict(net)
   safe_save(state_dict, "model.safetensors")
+  plt.plot([loss.numpy() for loss in loss_list])
+  plt.xlabel("steps")
+  plt.ylabel("loss")
+  plt.title("loss/step")
+  plt.savefig("loss")
+
 
 def inference():
   index = randint(0, TEST_IM.shape[0])
@@ -68,7 +76,7 @@ def inference():
   print(f"actual label: {example_label}, guess: {prediction}")
   plt.imshow(example.numpy().reshape((28,28)))
   plt.savefig("plot")
-  
+
 if __name__ == "__main__":
-  # train(steps=1000)
+  train(steps=1000)
   inference()
