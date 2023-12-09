cd0aed2ec316d0f05a7b9a1e383cedfe0af0d10f
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2023-12-08 17:10:08 -0800

    updating tinygrad

--- new_mnist.py
+++ new_mnist.py
@@ -20,18 +20,6 @@ class TinyNet:
 def train(steps: int, samp=np.random.randint(0, TRAIN_IM.shape[0], size=(64))):
   model = TinyNet()
   opt = nn.optim.SGD(nn.state.get_parameters(model), lr=3e-4)
-  with Tensor.train():
-    for step in range(steps):
-      batch, labels = Tensor(TRAIN_IM[samp], requires_grad=False), Tensor(TRAIN_LAB[samp], requires_grad=False)
-      opt.zero_grad()
-      out = model(batch)
-      loss = out.sparse_categorical_crossentropy(labels).backward()
-      opt.step()
-      print(loss.numpy())
-      # pred = out.argmax(axis=-1) # this basically return the model's prediction 
-      # acc = (pred == labels).mean() # i'm having a really hard time understanding what's going on here
-      # if step % 100 == 0:
-      #   print(f"Step {step+1} | Loss: {loss.numpy()} | Accuracy: {acc.numpy() * 100}") # 
 
 
 
