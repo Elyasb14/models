7b1d3389bc26f6ddc538271204645c86f93f4962
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2023-12-09 12:04:18 -0800

    idk why my new_mnist is acting weird

--- new_mnist.py
+++ new_mnist.py
@@ -1,14 +1,13 @@
-from helpers import load_mnist
+from helpers import load_mnist, load_fashion
 from tinygrad.tensor import Tensor
 from tinygrad.helpers import dtypes
 from tinygrad.nn import Linear
 from tinygrad.nn.optim import SGD
-from tinygrad.nn.state import get_parameters
+from tinygrad.nn.state import get_parameters, get_state_dict, safe_save
 from random import randint
-from tinygrad.jit import TinyJit
 from tinygrad.helpers import Timing
 
-TEST_IM, TEST_LAB, TRAIN_IM, TRAIN_LAB = load_mnist(tensors=True)
+TEST_IM, TEST_LAB, TRAIN_IM, TRAIN_LAB = load_fashion(tensors=True)
 
 def sparse_categorical_crossentropy(self: Tensor, Y: Tensor, ignore_index=-1) -> Tensor:
     loss_mask = Y != ignore_index
@@ -30,12 +29,11 @@ class TinyNet:
 model = TinyNet()
 opt = SGD(get_parameters(model), lr=3e-4)
 
-@TinyJit
 def train(steps):
   losses = []
   with Tensor.train():
     for i in range(steps):
-      samp = Tensor([randint(0, TEST_IM.shape[0]) for num in range(64)])
+      samp = Tensor([randint(0, TEST_IM.shape[0]) for _ in range(64)])
       batch, labels = TRAIN_IM[samp], TRAIN_LAB[samp]
       opt.zero_grad()
       loss = model(batch).sparse_categorical_crossentropy(labels).backward()
@@ -44,23 +42,15 @@ def train(steps):
       pred = model(batch).argmax(axis=-1); acc = (pred == labels).mean() # what is argmax doing
       if i % 100 == 0:
         print(f"Step {i+1} | Loss: {loss.numpy()} | Accuracy: {acc.numpy() * 100}")
+    state_dict = get_state_dict(model)
+    safe_save(state_dict, "new_model.safetensors")
 
-@TinyJit
-def get_test_acc() -> Tensor: return ((model(TEST_IM).argmax(axis=1) == TEST_LAB).mean()*100).realize()
+def get_test_acc() -> Tensor: return ((model(TEST_IM).argmax(axis=1) == TEST_LAB).mean()*100).numpy()
 
-@TinyJit
-def evaluate(steps):
+def evaluate():
   with Timing("Time: "):
-    avg_acc = 0
-    for step in range(steps):
-      samp = Tensor([randint(0, TEST_IM.shape[0]) for num in range(64)])
-      batch = TEST_IM[samp]
-      labels = TEST_LAB[samp]
-      pred = model(batch).argmax(axis=-1)
-      avg_acc += 
-    print(f"Test Accuracy: {avg_acc/steps}")
+    print(f"Test Accuracy: {get_test_acc()}")
 
 if __name__ == "__main__":
   train(1000)
-  evaluate(1000)
-
+  evaluate()
