95d6eb4435a6ddb65af257e9542f08b2d587d56d
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2023-12-09 18:55:43 -0800

    loading mnist as tensors was slower for whatever reason

--- helpers.py
+++ helpers.py
@@ -25,9 +25,10 @@ def load_fashion(tensors=False) -> Tuple[np.ndarray]:
   with open(f"data/fashionmnist/{data_dir[1]}", "rb") as f: TEST_LAB = parse(f.read())[8:]
   with open(f"data/fashionmnist/{data_dir[2]}", "rb") as f: TRAIN_IM = parse(f.read())[0x10:].reshape((-1, 28*28)).astype(np.float32)
   with open(f"data/fashionmnist/{data_dir[3]}", "rb") as f: TRAIN_LAB = parse(f.read())[8:]
-  if tensors: return Tensor(TRAIN_IM), Tensor(TRAIN_LAB), Tensor(TEST_IM), Tensor(TEST_LAB)   # noqa: E701
+  if tensors: return Tensor(TRAIN_IM, requires_grad=False), Tensor(TRAIN_LAB, requires_grad=False), Tensor(TEST_IM, requires_grad=False), Tensor(TEST_LAB, requires_grad=False)   # noqa: E701
   return TEST_IM, TEST_LAB, TRAIN_IM, TRAIN_LAB
 
+# TODO: why was it slower when I called this with tensors=True
 def load_mnist(tensors=False) -> Tuple[np.ndarray]:
   def parse(file): return np.frombuffer(file, dtype=np.uint8).copy() # gives 1d array of 64 random integers
   data_dir = sorted(os.listdir("data/mnist"))
@@ -35,5 +36,5 @@ def load_mnist(tensors=False) -> Tuple[np.ndarray]:
   with open(f"data/mnist/{data_dir[1]}", "rb") as f: TEST_LAB = parse(f.read())[8:]
   with open(f"data/mnist/{data_dir[2]}", "rb") as f: TRAIN_IM = parse(f.read())[0x10:].reshape((-1, 28*28)).astype(np.float32)
   with open(f"data/mnist/{data_dir[3]}", "rb") as f: TRAIN_LAB = parse(f.read())[8:]
-  if tensors: return Tensor(TEST_IM), Tensor(TEST_LAB), Tensor(TRAIN_IM), Tensor(TRAIN_LAB)   # noqa: E701
+  if tensors: return Tensor(TEST_IM, requires_grad=False), Tensor(TEST_LAB, requires_grad=False), Tensor(TRAIN_IM, requires_grad=False), Tensor(TRAIN_LAB, requires_grad=False)   # noqa: E701
   return TEST_IM, TEST_LAB, TRAIN_IM, TRAIN_LAB
