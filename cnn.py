4f3e4b76da584f8e088fed69d5a90a76b8aa17eb
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2023-12-10 13:38:32 -0800

    got cnn working on both mnist datasets

--- cnn.py
+++ cnn.py
@@ -1,4 +1,4 @@
-from helpers import load_mnist, plot_loss, load_fashion
+from helpers import plot_loss, load_fashion
 from tinygrad.nn import Conv2d, BatchNorm2d, Linear
 from tinygrad.nn.optim import Adam
 from tinygrad.nn.state import get_parameters, safe_save, get_state_dict, safe_load, load_state_dict
@@ -6,7 +6,7 @@ from tinygrad import Tensor
 from tqdm import trange
 from tinygrad.jit import TinyJit
 
-TEST_IM, TEST_LAB, TRAIN_IM, TRAIN_LAB = load_mnist(tensors=True)
+TEST_IM, TEST_LAB, TRAIN_IM, TRAIN_LAB = load_fashion(tensors=True)
 
 class CNN:
     def __init__(self):
@@ -35,7 +35,7 @@ def train(steps):
             opt.zero_grad()
             loss = model(batch).sparse_categorical_crossentropy(labels).backward()
             opt.step()
-            t.set_description(f"loss: {loss.item()}")
+            t.set_description(f"loss: {loss.item():6.2f}")
             losses.append(loss)
     safe_save(get_state_dict(model), "models/cnn.safetensors")
     plot_loss(losses)
@@ -48,17 +48,16 @@ def evaluate(steps):
         test_pred, labels  = model(TEST_IM[samp]).argmax(axis=1), TEST_LAB[samp]
         acc = (test_pred == labels).mean()*100
         avg_acc += acc.item()
-        t.set_description(f"avg acc: {avg_acc/steps}")
+        t.set_description(f"avg acc: {avg_acc/steps:5.2f}%")
 
-@TinyJit
 def inference():
-  samp = Tensor.randint(1, high=TRAIN_IM.shape[0])
-  weights = safe_load("models/cnn.safetensors")
-  load_state_dict(model, weights)
-  pred, label = model(TEST_IM[samp]).argmax(axis=1), TEST_LAB[samp]
-  print(f"model's prediction: {pred.numpy()}, actual label: {label.numpy()}")
+    model, weights = CNN(), safe_load("models/cnn.safetensors")
+    sample = Tensor.randint(1, high=TEST_IM.shape[0])
+    load_state_dict(model, weights)
+    out, label = model(TEST_IM[sample]).argmax(axis=1), TEST_LAB[sample]
+    print(f"model pred: {out.realize().item()}, actual label: {label.realize().item()}")
 
 if __name__ == "__main__":
-    # train(70)
-    # evaluate(70)
+    train(500)
+    evaluate(500)
     inference()
