186ff19f1f2deb939c9a65d576b425593e444547
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2023-12-24 17:41:42 +1300

    readme

---
+++ README.md
@@ -0,0 +1,34 @@
+# Models implemented in tinygrad
+
+learning ml using [tinygrad](https://github.com/tinygrad/tinygrad).
+
+## QUICKSTART
+
+you can technically run these models directly on the cpu, but they will be painfilly slow. the general suggestion is to make sure `clinfo` works, or if on a mac, use the metal backend. clone the repo, update tinygrad to the head of the master branch, and train an mlp on mnist using the gpu (opencl) backend by running the following:
+
+```bash
+git clone git@github.com:Elyasb14/models.git
+./update_tinygrad.sh
+GPU=1 python3 mlp.py train
+```
+
+this will save the state of the model in `models/mlp.safetensors`. infer by running `GPU=1 python3 mlp.py infer`.
+
+## MODELS
+
+mlp.py: the most basic of all nns, an mlp. this trains mnist to around 93%, and fashion mnist to about 80%. it consists of 2 linear layers with a leakyrelu in between them. 
+
+## TODO
+
+- [ ] implement some makemore models
+- [ ] get some onnx model and run it in pure tinygrad
+- [ ] make data downloaders for both mnist and fashionmnist 
+
+## REFERENCES
+
+- [tinygrad](https://github.com/tinygrad/tinygrad)
+- [makemore](https://github.com/karpathy/makemore)
+
+## LESSONS LEARNED
+
+- when training fashion mnist on cnn.py, simply raising the training steps doesn't really contribute to notably better model accuracy. the loss function may get driven down a lot, but this doesn't necessarily contribute to a better model.
--- tinygrad/runtime/ops_disk.py
+++ tinygrad/runtime/ops_disk.py
@@ -1,6 +1,4 @@
-import os, mmap
-try: import _posixshmem
-except Exception: pass
+import os, mmap, _posixshmem
 from typing import Callable, Dict, Tuple
 from tinygrad.helpers import prod, DType, OSX, dtypes
 from tinygrad.device import Interpreted, Allocator
@@ -13,9 +11,13 @@ class UnderlyingDiskBuffer:
     if self.fd: self.fd.close()
 
 class DiskBuffer:
-  def __init__(self, ud:UnderlyingDiskBuffer, size:int, dtype:DType=dtypes.uint8, offset=0): self.ud, self.size, self.dtype, self.offset = ud, size, dtype, offset
+  def __init__(self, ud:UnderlyingDiskBuffer, size:int, dtype:DType=dtypes.uint8, offset=0):
+    self.ud, self.size, self.dtype, self.offset = ud, size, dtype, offset
   def __repr__(self): return f"<DiskBuffer size={self.size} dtype={self.dtype} offset={self.offset}>"
-  def cast(self, arg:Tuple[DType, bool]): return DiskBuffer(self.ud, self.size, arg[0], offset=self.offset)
+  def cast(self, arg:Tuple[DType, bool]):
+    # TODO: support shape changing bitcast
+    #assert arg[1], "DiskTensor only supports bitcast"
+    return DiskBuffer(self.ud, self.size, arg[0], offset=self.offset)
   def as_strided(self, arg):
     assert strides_for_shape(arg[0]) == arg[1], "disk tensors don't support strides"
     return DiskBuffer(self.ud, prod(arg[0]), self.dtype, offset=self.offset+arg[2]*self.dtype.itemsize)
@@ -23,21 +25,15 @@ class DiskBuffer:
 
 disk_fxn_for_op: Dict[Op, Callable] = { UnaryOps.CAST: DiskBuffer.cast, MovementOps.AS_STRIDED: DiskBuffer.as_strided }
 
-MAP_LOCKED, MAP_POPULATE = 0x2000, 0x008000
+MAP_LOCKED, MAP_POPULATE = 0 if OSX else 0x2000, getattr(mmap, "MAP_POPULATE", 0 if OSX else 0x008000)
 class DiskAllocator(Allocator):
   def __init__(self, device): self.device = device
   def _alloc(self, size):
     if str(self.device).startswith("shm:"):
-      if OSX:
-        with open(f"/tmp/shm_{self.device[4:]}", "w+b") as f:
-          f.truncate(size)
-          shm = mmap.mmap(f.fileno(), size, flags=mmap.MAP_SHARED)
-      else:
-        fd = _posixshmem.shm_open(self.device[4:], os.O_RDWR, 0o600)
-        # TODO: these flags are somewhat platform specific, but python doesn't expose the ones we need
-        shm = mmap.mmap(fd, size, flags=mmap.MAP_SHARED | MAP_LOCKED | MAP_POPULATE)
-        shm.madvise(mmap.MADV_HUGEPAGE)     # type: ignore   # not on OSX
-        os.close(fd)
+      fd = _posixshmem.shm_open("/"+self.device[4:].lstrip("/"), os.O_RDWR, 0o600)
+      shm = mmap.mmap(fd, size, flags=mmap.MAP_SHARED | MAP_POPULATE | MAP_LOCKED)
+      if (hp := getattr(mmap, "MADV_HUGEPAGE", None)) is not None: shm.madvise(hp) # type: ignore
+      os.close(fd)
       buf = UnderlyingDiskBuffer(None, shm)
     else:
       f = open(self.device, "a+b")
